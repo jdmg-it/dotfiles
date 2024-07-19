59d5475e535cc4db4a4985abba9cdc10d31ac0f7
José David Montoya Gallardo <jdmg6@alu.ua.es>
2024-07-19 12:46:34 +0200

    Add warp themes

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,21 +1,29 @@
 neofetch
+
 # Linux
 if [[ $(uname) == "Linux" ]]; then
-	export BROWSER="wslview"
-	export PATH="/home/jd/.local/share/fnm:$PATH"
+	# fnm
+	FNM_PATH="/home/jd/.local/share/fnm"
+	if [ -d "$FNM_PATH" ]; then
+	  export PATH="/home/jd/.local/share/fnm:$PATH"
+	  eval "`fnm env`"
+	fi
+
+#MacOS
+elif [[ $(uname) == "Darwin" ]]; then
+	PATH=~/.console-ninja/.bin:$PATH
+	# pnpm
+	export PNPM_HOME="/Users/jd/Library/pnpm"
+	case ":$PATH:" in
+	  *":$PNPM_HOME:"*) ;;
+	  *) export PATH="$PNPM_HOME:$PATH" ;;
+	esac
+	# pnpm end
+	
+	eval "$(fnm env --use-on-cd)"
 fi
 
 eval "$(starship init zsh)"
-eval "$(fnm env --use-on-cd)"
 
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
-
-PATH=~/.console-ninja/.bin:$PATH
-# pnpm
-export PNPM_HOME="/Users/jd/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-# pnpm end
